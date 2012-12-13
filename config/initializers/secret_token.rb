f8507d8aa82f59ea34cfe22097e60f5c8b3c3878
Hamed Ramezanian <iceage2098@gmail.com>
2012-12-13 12:47:38 +0330

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarsiTextcaptcha::Application.config.secret_token = 'd70dc7572bdf5c6b8f087c74bb5470b36bc00549419ee7df375b36e8e5c92500f9be172ab72cb7189e5ee6be446d499e632920b5f8c6ef586816c9da50e191c9'
