8376f2c7096a8aa4f229e9580fce79e4a7f94569
Hamed Ramezanian <iceage2098@gmail.com>
2012-12-29 22:11:59 +0330

    changes ui and some translation from textcaptcha.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :load_number_of_approved_questions
+
+  private
+
+  def load_number_of_approved_questions
+    @number_of_approved_questions = Question.approved.count
+  end
 end
