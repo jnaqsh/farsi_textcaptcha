1265ec86958e59c4aa0db2add4ab615a7626c3ba
Hamed Ramezanian <iceage2098@gmail.com>
2012-12-30 17:40:24 +0330

    replaces append with after

--- app/assets/javascripts/nested_form_overrides.js
+++ app/assets/javascripts/nested_form_overrides.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   window.nestedFormEvents.insertFields = function(content, assoc, link) {
-    return $(link).closest('form').find('#answers').after($(content));
+    return $(link).closest('form').find('#answers').append($(content));
   }
 });
