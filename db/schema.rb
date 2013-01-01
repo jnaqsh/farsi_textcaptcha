dce6c6d561eef3f3f1bbf0773572fd6e980e6ec2
Hamed Ramezanian <iceage2098@gmail.com>
2013-01-01 18:26:58 +0330

    adds index on answer table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121231125220) do
+ActiveRecord::Schema.define(:version => 20130101145334) do
 
   create_table "answers", :force => true do |t|
     t.string   "answer"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(:version => 20121231125220) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  add_index "answers", ["question_id"], :name => "index_answers_on_question_id"
 
   create_table "questions", :force => true do |t|
     t.string   "question"
