a79dad1afc16f65df6299643f52670ae714f6df5
Hamed Ramezanian <iceage2098@gmail.com>
2013-01-06 16:37:47 +0330

    adds sendmail as delivery method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,13 @@ FarsiTextcaptcha::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+    :location => '/usr/sbin/sendmail',
+    :arguments => '-i -t'
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
