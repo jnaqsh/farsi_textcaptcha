197278ff0801e4d5ec7857161b6b77884488131e
Hamed Ramezanian <iceage2098@gmail.com>
2013-01-07 12:48:29 +0330

    excludes bootstrap files that not in used

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
+//= require twitter/bootstrap/bootstrap-collapse
 //= require jquery_nested_form
 //= require nested_form_overrides
 //= require prettify
